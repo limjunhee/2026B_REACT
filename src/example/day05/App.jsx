d1ee6698f2512fe4a1f3826ae694109fc6ed1ed8
limjunhee <ski725400@gmail.com>
2026-09-17 11:10:03 +0900

    Day5_2

--- src/example/day05/App.jsx
+++ src/example/day05/App.jsx
@@ -1,10 +1,15 @@
 // p171 생명주기 실습
 
+import { Route, Routes } from "react-router-dom";
 import TopNavi from "./TopNavi";
+import Lifecycle from "./Lifecycle";
 
 export default function App(props) {
     return (<>
         생명주기 실습
         <TopNavi></TopNavi>
+        <Routes>
+            <Route path="/" element={<Lifecycle/>}></Route>
+        </Routes>
     </>)
 }
--- src/example/day05/Lifecycle.jsx
+++ src/example/day05/Lifecycle.jsx
@@ -1,3 +1,41 @@
+import { useEffect, useState } from "react"
+
+function MoveBox(props){
+    const [position, setPosition] = useState(props.initPosition);
+    const [leftCount, setLeftCount ] = useState( 1 );
+    const boxStyle = {
+        backgroundColor : 'red' , position: 'relative', textAlign : 'center',
+        width : '100px', height : " 100px", margin : "10px", lineHeight: '100px',
+        left : `${position}px`
+    }
+
+    const moveLeft = ( ) =>{
+        setPosition ( () => position - 20 );
+        setLeftCount ( () => leftCount + 1 );
+
+    }
+
+    const moveRight = ( ) => {
+        setPosition ( ( ) => position + 20 )
+    }
+
+    useEffect( () => {
+        console.log("useEffect 실행 --> 마운트")
+        return () => {
+            console.log("useEffect 실행 --> 언마운트")
+        }
+    } );
+
+    console.log( 'return 실행 --> 렌더링(버튼)')
+    return(<>
+        <div style={boxStyle}>{leftCount}</div>
+        <button onClick={moveLeft}>좌측이동</button>
+        <button onClick={moveRight}>우측이동</button>
+    </>)
+}
 export default function Lifecycle(props){
-    
+    return(<>
+        <h2>React Hook : useEffect</h2>
+        <MoveBox initPosition={50}></MoveBox> {/* 박스 최초 위치 : left:50px*/}
+    </>)
 }
